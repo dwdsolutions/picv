a526522113cbf7adb7c55d138b0e0039f6baa3ee
William Díaz <leprosys@gmail.com>
2013-01-26 15:24:52 -0600

    first commit

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Picv::Application.config.secret_token = 'e2831043bef1625b85d23ab5e075e103dafabbf7bb99c6b8565c899db83c62b5c1313db3bfc3493c5f02f55d826c80fff72887181f540fb50b587469153b82d2'

ba0678fe913aed96c15a2cdeb517a6cea11636e3
Carlos Aguilar <caguilar@dwdandsolutions.com>
2013-01-27 11:23:38 -0600

    Agregando la capacidad de crear expediente con sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -6,6 +6,34 @@ class SmsController < ApplicationController
 
 		if @body.upcase == "AYUDA"
 			@message = "Por favor, envianos tu informacion con el siguiente formato: Nombre, Numero de DUI, Sexo"
+		elsif @body.split(', ').length == 3
+			sexo = {
+				'masculino' => "masculino",
+				'femenino' => "femenino",
+				'hombre' => "masculino",
+				'mujer' => "femenino",
+				'm' => "masculino",
+				'f' => "femenino",
+			}
+			datos_usuario = @body.split(', ')
+			expediente = Expediente.new
+			nombre = datos_usuario.split
+			if nombre.length == 4
+				expediente.nombre = nombre[0].concat(" ").concat(nombre[1])
+				expediente.apellido1 = nombre[2]
+				expediente.apellido2 = nombre[3]
+			elsif nombre.length == 3
+				expediente.nombre = nombre[0].concat(" ").concat(nombre[1])
+				expediente.apellido1 = nombre[2]
+			else
+				expediente.nombre = nombre[0]
+				expediente.apellido1 = nombre[1]
+			end
+			expediente.telefono = @from
+			expediente.documento_identidad = datos_usuario[1]
+			expediente.sexo = sexo[datos_usuario[2].downcase]
+			expediente.save()
+					
 		else
 			@message = "Comando no reconocido!. Comandos reconocidos: AYUDA"
 		end

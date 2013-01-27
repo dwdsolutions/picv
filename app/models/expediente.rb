fced20ceb76259885152f8d1c10db498125368e6
Carlos Aguilar <caguilar@dwdandsolutions.com>
2013-01-26 18:49:46 -0600

    Agregados campos al modelo expediente

--- app/models/expediente.rb
+++ app/models/expediente.rb
@@ -1,5 +1,7 @@
 class Expediente < ActiveRecord::Base
   belongs_to :municipio
   attr_accessible :apellido1, :apellido2, :documento_identidad, :edad, :email, :nombre, 
-  				  :sexo, :telefono
+  				  :sexo, :telefono, :zona, :ocupacion, :lugar_trabajo, :telefono_trabajo,
+  				  :municipio_trabajo_id, :nivel_educativo, :estado_familiar, :sostenimiento_familiar,
+  				  :discapacidad_fisica, :discapacidad_mental
 end
